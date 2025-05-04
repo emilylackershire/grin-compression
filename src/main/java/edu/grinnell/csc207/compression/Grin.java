78c0d8b7bb0cb62f16beb2fe8af92535daacbef7
Emily <lackersh@grinnell.edu>
2025-05-03 19:24:19 -0500

    putting arguments back in

--- src/main/java/edu/grinnell/csc207/compression/Grin.java
+++ src/main/java/edu/grinnell/csc207/compression/Grin.java
@@ -72,9 +72,9 @@ public class Grin {
      */
     public static void main(String[] args) throws IOException {
         System.out.println("Usage: java Grin <encode|decode> <infile> <outfile>");
-        String code = "encode"; //args[0];
-        String infile = "huffman-example.grin"; //args[1];
-        String outfile = "huffman-example.grin"; //args[2];
+        String code = args[0];
+        String infile = args[1];
+        String outfile = args[2];
         if (code.equals("encode")) {
             encode(infile, outfile);
         } else if (code.equals("decode")) {
